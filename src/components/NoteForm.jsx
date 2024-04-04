0d7823c5391f458b21e8ace59c1b3bbf45437c95
Aung Ye Htet <aung.atg1738@gmail.com>
2024-04-04 21:03:27 +0630

    Formik Install And create custom error message

--- src/components/NoteForm.jsx
+++ src/components/NoteForm.jsx
@@ -1,8 +1,37 @@
 import { ArrowLeftEndOnRectangleIcon } from "@heroicons/react/24/solid";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import { Formik, Field, Form } from "formik";
+
+//formik custom error message
+import CustomStyleErrorMessage from "./CustomStyleErrorMessage";
 
 const NoteForm = ({ isCreate }) => {
+  const initialValues = {
+    title: "",
+    content: "",
+  };
+
+  //This method has initial values
+  const validate = (values) => {
+    const errors = {};
+
+    if (values.title.trim().length < 10) {
+      errors.title = "Title must be at least 10 characters";
+    }
+
+    if (values.content.trim().length < 10) {
+      errors.content = "Content must be at least 10 characters";
+    }
+
+    return errors;
+  };
+
+  // In this method (values) is the form input box
+  const submitHandler = (values) => {
+    console.log(values);
+  };
+
   return (
     <section>
       <div className="flex items-center justify-between">
@@ -13,34 +42,49 @@ const NoteForm = ({ isCreate }) => {
           <ArrowLeftEndOnRectangleIcon width={22} />
         </Link>
       </div>
-      <form>
-        <div className="mb-3">
-          <label htmlFor="title" className=" font-medium block">
-            Note Title
-          </label>
-          <input
-            type="text"
-            name="title"
-            id="title"
-            className=" text-lg border-2 border-teal-600 py-1 w-full rounded-lg"
-          />
-        </div>
-        <div className="">
-          <label htmlFor="description" className=" font-medium block">
-            Note Description
-          </label>
-          <textarea
-            rows={10}
-            type="text"
-            name="description"
-            id="description"
-            className=" text-lg border-2 border-teal-600 py-1 w-full rounded-lg"
-          />
-        </div>
-        <button className="text-white bg-teal-600 py-3 font-medium w-full">
-          Save Note
-        </button>
-      </form>
+
+      <Formik
+        initialValues={initialValues}
+        validate={validate}
+        onSubmit={submitHandler}
+      >
+        {({ errors, touched }) => (
+          <Form>
+            <div className="mb-3">
+              <label htmlFor="title" className=" font-medium block">
+                Note Title
+              </label>
+              <Field
+                type="text"
+                name="title"
+                id="title"
+                className=" text-lg border-2 border-teal-600 py-1 w-full rounded-lg"
+              />
+              <CustomStyleErrorMessage name="title" />
+            </div>
+            <div className="">
+              <label htmlFor="content" className=" font-medium block">
+                Note content
+              </label>
+              <Field
+                as="textarea"
+                rows={10}
+                type="text"
+                name="content"
+                id="content"
+                className=" text-lg border-2 border-teal-600 py-1 w-full rounded-lg resize-none"
+              />
+              <CustomStyleErrorMessage name="content" />
+            </div>
+            <button
+              type="submit"
+              className="text-white bg-teal-600 py-3 font-medium w-full"
+            >
+              Save Note
+            </button>
+          </Form>
+        )}
+      </Formik>
     </section>
   );
 };
