47c0f3ed14d28e5eb8c60c6619a3cc96777b05bf
Aung Ye Htet <aung.atg1738@gmail.com>
2024-04-07 22:13:37 +0630

    IndexUpdate and Nav Update

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,10 +2,15 @@ import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
-    <nav className=" bg-slate-200 py-4 px-10">
+    <nav className=" bg-slate-200 py-4 px-10 flex items-center justify-between font-mono">
       <Link to={"/"}>
         <h1 className=" text-teal-600 font-bold text-4xl">SANote.io</h1>
       </Link>
+      <div>
+        <Link to={"/create"} className="text-teal-600 font-medium">
+          Share Notes
+        </Link>
+      </div>
     </nav>
   );
 };
