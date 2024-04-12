bfd4e13edfc0ff28439fb9c8f194dc575968060a
Aung Ye Htet <aung.atg1738@gmail.com>
2024-04-12 21:54:17 +0630

    AddPagination

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,18 +7,35 @@ import "react-toastify/dist/ReactToastify.css";
 const Index = () => {
   const [notes, setNotes] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
 
-  const getNotesFromAPI = async () => {
+  const getNotesFromAPI = async (pageNum) => {
     setLoading(true);
-    const response = await fetch(`${import.meta.env.VITE_API}/notes`);
-    const data = await response.json();
-    setNotes(data);
+    const response = await fetch(
+      `${import.meta.env.VITE_API}/notes?page=${pageNum}`
+    );
+    const { notes, totalPages } = await response.json();
+    setTotalPages(totalPages);
+    setNotes(notes);
     setLoading(false);
   };
 
   useEffect(() => {
-    getNotesFromAPI();
-  }, []);
+    getNotesFromAPI(currentPage);
+  }, [currentPage]);
+
+  const handlePreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
 
   const customAlert = (message) => {
     toast.success(message, {
@@ -35,7 +52,7 @@ const Index = () => {
   };
 
   return (
-    <section className=" flex gap-6 px-10 mt-10 flex-wrap mx-auto w-full justify-center">
+    <section className="flex gap-6 px-10 mt-10 flex-wrap mx-auto w-full justify-center">
       {!loading && notes.length > 0 ? (
         <>
           {notes.map((note) => (
@@ -46,6 +63,26 @@ const Index = () => {
               customAlert={customAlert}
             />
           ))}
+          <div className="w-full flex items-center justify-center gap-3">
+            {currentPage > 1 && (
+              <button
+                type="button"
+                className="text-white font-medium bg-teal-600 px-3 py-1"
+                onClick={handlePreviousPage}
+              >
+                Previous
+              </button>
+            )}
+            {currentPage < totalPages && (
+              <button
+                type="button"
+                className="text-white font-medium bg-teal-600 px-3 py-1"
+                onClick={handleNextPage}
+              >
+                Next
+              </button>
+            )}
+          </div>
         </>
       ) : (
         <div className="flex justify-center items-center w-full">
