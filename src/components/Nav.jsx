c35b84c9107d9df4933266bdfc203217a8e985f7
Aung Ye Htet <aung.atg1738@gmail.com>
2024-04-17 21:53:49 +0630

    ComponentUpdate

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,31 +3,12 @@ import { useContext } from "react";
 
 import { UserContext } from "../contexts/UserContext";
 
-import { useEffect, useState } from "react";
-import { Navigate } from "react-router-dom";
-
 const Nav = () => {
-  const { token } = useContext(UserContext);
-  const [redirect, setRedirect] = useState(false);
+  const { token, updateToken } = useContext(UserContext);
 
-  const isLogin = async () => {
-    const response = await fetch(`${import.meta.env.VITE_API}/status`, {
-      headers: {
-        Authorization: `Bearer ${token.token}`,
-      },
-    });
-    if (response.status === 401) {
-      setRedirect(true);
-    }
+  const logoutHandler = () => {
+    updateToken(null);
   };
-
-  useEffect(() => {
-    isLogin();
-  });
-
-  if (redirect) {
-    return <Navigate to="/login" />;
-  }
 
   return (
     <nav className=" bg-slate-200 py-4 px-10 flex items-center justify-between font-mono">
@@ -36,9 +17,17 @@ const Nav = () => {
       </Link>
       <div className="flex gap-3">
         {token ? (
-          <Link to={"/create"} className="text-teal-600 font-medium">
-            Share Notes
-          </Link>
+          <>
+            <Link to={"/create"} className="text-teal-600 font-medium">
+              Share Notes
+            </Link>
+            <p
+              className="text-teal-600 font-medium cursor-pointer"
+              onClick={logoutHandler}
+            >
+              Logout
+            </p>
+          </>
         ) : (
           <>
             <Link to={"/login"} className="text-teal-600 font-medium">
