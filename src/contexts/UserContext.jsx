a489f5689be21e069ead72cf2e3c65615ebdde98
Aung Ye Htet <aung.atg1738@gmail.com>
2024-04-17 21:19:12 +0630

    StoreToken

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -1,13 +1,28 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import PropTypes from "prop-types";
 
 export const UserContext = createContext();
 
 export const UserContextProvider = ({ children }) => {
-  const [token, setToken] = useState(null);
+  const [token, setToken] = useState(() => {
+    const tokenStore = localStorage.getItem("token");
+    return tokenStore ? JSON.parse(tokenStore) : null;
+  });
+
+  const updateToken = (JWTtoken) => {
+    localStorage.setItem("token", JSON.stringify(JWTtoken));
+    setToken(JWTtoken);
+  };
+
+  useEffect(() => {
+    const tokenStore = localStorage.getItem("token");
+    if (tokenStore) {
+      setToken(JSON.parse(tokenStore));
+    }
+  }, []);
 
   return (
-    <UserContext.Provider value={{ token, setToken }}>
+    <UserContext.Provider value={{ token, updateToken }}>
       {children}
     </UserContext.Provider>
   );
