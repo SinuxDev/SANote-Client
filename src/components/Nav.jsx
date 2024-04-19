efddc51d4d971f8f8b0db3bbbc07032e2b7e8671
Aung Ye Htet <aung.atg1738@gmail.com>
2024-04-19 13:17:54 +0630

    NavBarUpdate AddUserName

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -11,34 +11,42 @@ const Nav = () => {
   };
 
   return (
-    <nav className=" bg-slate-200 py-4 px-10 flex items-center justify-between font-mono">
-      <Link to={"/"}>
-        <h1 className=" text-teal-600 font-bold text-4xl">SANote.io</h1>
-      </Link>
-      <div className="flex gap-3">
-        {token ? (
-          <>
-            <Link to={"/create"} className="text-teal-600 font-medium">
-              Share Notes
-            </Link>
-            <p
-              className="text-teal-600 font-medium cursor-pointer"
-              onClick={logoutHandler}
-            >
-              Logout
-            </p>
-          </>
-        ) : (
-          <>
-            <Link to={"/login"} className="text-teal-600 font-medium">
-              Login
-            </Link>
-            <Link to={"/register"} className="text-teal-600 font-medium">
-              Register
-            </Link>
-          </>
-        )}
+    <nav className="bg-slate-200 py-4 px-10 font-mono ">
+      <div className=" flex items-center justify-between ">
+        <Link to={"/"}>
+          <h1 className=" text-teal-600 font-bold text-4xl">SANote.io</h1>
+        </Link>
+        <div className="flex gap-3">
+          {token ? (
+            <>
+              <Link to={"/create"} className="text-teal-600 font-medium">
+                Share Notes
+              </Link>
+              <p
+                className="text-teal-600 font-medium cursor-pointer"
+                onClick={logoutHandler}
+              >
+                Logout
+              </p>
+            </>
+          ) : (
+            <>
+              <Link to={"/login"} className="text-teal-600 font-medium">
+                Login
+              </Link>
+              <Link to={"/register"} className="text-teal-600 font-medium">
+                Register
+              </Link>
+            </>
+          )}
+        </div>
       </div>
+
+      {token && token.userName && (
+        <p className="text-right text-sm text-teal-600">
+          <span className="font-semibold">Login as </span> {token.userName}{" "}
+        </p>
+      )}
     </nav>
   );
 };
